cfabe2c1b76a16fc34d6088227c0e096d23fe892
Pebaz <pebazium@gmail.com>
2023-06-25 13:18:12 -0700

    save

--- tidbytes/idiomatic.py
+++ tidbytes/idiomatic.py
@@ -350,6 +350,11 @@ import struct
 
 float.hex
 
+value = []
+isinstance(value, list[int])
+isinstance(value, [u8, u16, u32, u64, i8, i16, i32, i64, f32, f64])
+isinstance(value, tuple)
+
 
 # * Getting
 
