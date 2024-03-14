bba6812092ae7df9a5bbd33a6a2d4095ed706db7
Pebaz <pebazium@gmail.com>
2024-03-14 16:43:35 -0400

    Fix ruff lints

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,15 +1,11 @@
 import pytest
-from tidbytes.mem_types import (
-    MemException, u8, u16, u32, u64, i8, i16, i32, i64, f32, f64
-)
+from tidbytes.mem_types import MemException, i8, i16, i32, i64
 from tidbytes.idiomatic import Mem, Unsigned, Signed
 from tidbytes.codec import (
     range_unsigned, range_signed, is_in_range_signed, is_in_range_unsigned,
     from_natural_big_integer_signed, from_natural_big_integer_unsigned,
     from_numeric_big_integer_signed, from_numeric_big_integer_unsigned
 )
-
-from . import raises_exception, UN, Slice
 
 def test_smoke():
     """
@@ -38,36 +34,6 @@ def test_smoke():
     assert str(Mem[2](0)) == '00'
     assert str(Signed[2](-1)) == '11'
     assert str(Signed[2](-2)) == '10'
-
-    # It's odd, but the twos-complement counts up towards -1 (properly here)
-    # ! assert str(Signed[2](2)) == '10' <- This should error out -2 ..= 1
-    # ! This is the problem ^^^
-    # ! There needs to be checking even for the positive range:
-    # ! from_numeric_big_integer needs to check for signed/unsigned because the
-    # ! ranges are totally different
-    # Mem[2](2) is also wrong due to signedness issues with bit_len
-    # ? Does that mean codecs need to know origin AND destination?
-    # * bit_length has different range for signed and unsigned although this
-    # * isn't the case with other codec types right?
-    # ? from_numeric_big_integer_into_signed()
-    # * from_numeric_u32() says the signedness -> u32
-    # * from_numeric_signed_big_integer() has half range than unsigned
-    # * from_numeric_unsigned_big_integer() has full range, truncs null values
-    # Concerns: the goal of splitting these up is because I thought there would
-    # be generic versions of these but it turns out that the codec may have to
-    # know the destination type (Signed vs Unsigned). Is this true? <-
-    # In theory, the codec doesn't care how the bits are treated afterwards. It
-    # could just load them into memory according to a
-
-    # * For these two functions, the signedness impacts how the bit_length field
-    # * is interpreted:
-    # *     from_numeric_big_integer_signed
-    # *     from_numeric_big_integer_unsigned
-    # * from_ubig(), from_ibig()
-
-    # I think this is ok: these codecs are generic because the `_signed` and
-    # `_unsigned` postfix refers to how the `bit_length` field is interpreted
-    # and validated/verified.
 
     # assert str(Signed[2](2)) != '10', 'i2 has range -2 ..= 1'
     assert str(Signed[2](1)) == '01'
